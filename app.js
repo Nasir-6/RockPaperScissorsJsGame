db2191436d3c86e0b5fe6279858a5ac33c130096
Nasir <nasir-islam@hotmail.com>
2021-08-19 22:34:46 +0100

    Player Selection Correction implemented

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-console.log("Hello World!");
+inputCaseCorrector = (aString) => {
+    if ((typeof aString) === "string"){
+        let firstLetterCapitilised = aString[0].toUpperCase();
+        let restOfLetters = aString.slice(1).toLowerCase();
+        let caseCorrectedString = firstLetterCapitilised + restOfLetters 
+        return caseCorrectedString
+    } else{
+        return "This is not a string"
+    }
+
+}
+
 
  computerPlays = () => {
     
@@ -16,32 +27,27 @@ console.log("Hello World!");
 };
 
 
-
-
-// Player Selection
-
 playerSelection = () => {
     let playerChoice = window.prompt("Rock, Paper or Scissor? ");
-    console.log(playerChoice)
+    
+    let caseCorrectedInput = inputCaseCorrector(playerChoice);
+
+    switch(caseCorrectedInput) {
+        case "Rock":
+        case "Paper":
+        case "Scissors":
+          console.log(caseCorrectedInput)
+          break;
+        default:
+          alert("Not rock paper or scissors, please choose again!");
+          playerSelection();
+      }
+
+      return caseCorrectedInput;
 }
 
 
 
-// Take input in 
-// Case Correct it
-// Check if rock Paper or Scissors
-// Return player selection
 
-let inputCaseCorrector = (aString) => {
-    if ((typeof aString) === "string"){
-        let firstLetterCapitilised = aString[0].toUpperCase();
-        let restOfLetters = aString.slice(1).toLowerCase();
-        let caseCorrectedString = firstLetterCapitilised + restOfLetters 
-        return caseCorrectedString
-    } else{
-        return "This is not a string"
-    }
-
-}
 
 playerSelection();
