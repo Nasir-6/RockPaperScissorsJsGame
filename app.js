4f80677bbf52352a7361969ab3e95896cfb9e265
Nasir <nasir-islam@hotmail.com>
2021-08-19 21:54:58 +0100

    Created CPU random throw function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+ computerPlays = () => {
+    
+    let cpuChooses = Math.floor(Math.random() * 3);
+    if(cpuChooses === 0){
+        return "Rock"
+    } else if (cpuChooses === 1){
+        return "Paper"
+    } else if (cpuChooses === 2){
+        return "Scissors"
+    } else{
+        return "unCaught error"
+    }
+
+};
+
+
