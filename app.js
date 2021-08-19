e3341d37773eaef7c4444badaa0627ea90ebdd58
Nasir <nasir-islam@hotmail.com>
2021-08-19 23:21:05 +0100

    round of 5 game Plus some console log adjustment for game commentary

--- app.js
+++ app.js
@@ -51,27 +51,78 @@ playGame = (playerChoice, cpuChoice) => {
     console.log(playerChoice);
     console.log(cpuChoice)
 
+    let userResult; // 0 = lose, 1 = draw, 2 = win
+
     if (playerChoice === cpuChoice){
         console.log("Draw! You both chose " + playerChoice)
+        userResult = 1;
+
     } else if (playerChoice === "Rock" && cpuChoice === "Paper"){
         console.log("You lose! " + cpuChoice + " beats " + playerChoice)
+        userResult = 0;
     } else if (playerChoice === "Paper" && cpuChoice === "Scissors"){
         console.log("You lose! " + cpuChoice + " beats " + playerChoice)
+        userResult = 0;
+    
     } else if (playerChoice === "Scissors" && cpuChoice === "Rock"){
         console.log("You lose! " + cpuChoice + " beats " + playerChoice)
+        userResult = 0;
+
     } else if (playerChoice === "Rock" && cpuChoice === "Scissors"){
         console.log("You win! " + playerChoice + " beats " + cpuChoice)
+        userResult = 2;
+
     } else if (playerChoice === "Paper" && cpuChoice === "Rock"){
         console.log("You win! " + playerChoice + " beats " + cpuChoice)
+        userResult = 2;
+
     } else if (playerChoice === "Scissors" && cpuChoice === "Paper"){
         console.log("You win! " + playerChoice + " beats " + cpuChoice)
+        userResult = 2;
     } else{
         console.log("Unexpected error");
+        userResult = 400
     }
 
+    return userResult;
 }
 
 
+playRoundOfFive = () =>{
 
-playGame(playerSelection(), computerPlays())
+    let result;
+    let cpuScore = 0;
+    let playerScore = 0;
 
+    for(round = 1; round<=5; round++){
+        result = playGame(playerSelection(), computerPlays())
+        if(result === 0){cpuScore++}
+        else if (result === 2){playerScore++}
+        console.log(
+            `Round ${round} Current Score
+            You  -  CPU
+            ${playerScore}    -  ${cpuScore}`)
+    }
+    
+
+    
+    console.log(roundOfFiveResult(cpuScore, playerScore))
+
+}
+
+roundOfFiveResult = (cpuScore, playerScore) => {
+    let finalResult
+    if(playerScore > cpuScore){
+        finalResult = "You won " + playerScore + " - " + cpuScore 
+    } else if (playerScore < cpuScore){
+        finalResult = "You Lost " + playerScore + " - " + cpuScore 
+    } else if (playerScore === cpuScore){
+        finalResult = "You Drew " + playerScore + " - " + cpuScore 
+    } else {
+        finalResult = "uncaught error!! Your Score: " + playerScore + " CPU Score: " + cpuScore
+    }
+
+    return finalResult
+}
+
+playRoundOfFive()
