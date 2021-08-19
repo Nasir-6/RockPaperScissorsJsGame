9d28eaf447fd8d2c17354dd10a651db60f2c2bb4
Nasir <nasir-islam@hotmail.com>
2021-08-19 22:54:24 +0100

    Evaluate and log result of a single game between cpu and user

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ inputCaseCorrector = (aString) => {
 
 
 playerSelection = () => {
-    let playerChoice = window.prompt("Rock, Paper or Scissor? ");
+    let playerChoice = window.prompt("Rock, Paper or Scissors? ");
     
     let caseCorrectedInput = inputCaseCorrector(playerChoice);
 
@@ -36,18 +36,42 @@ playerSelection = () => {
         case "Rock":
         case "Paper":
         case "Scissors":
-          console.log(caseCorrectedInput)
+          
           break;
         default:
           alert("Not rock paper or scissors, please choose again!");
-          playerSelection();
+          caseCorrectedInput = playerSelection();
       }
 
       return caseCorrectedInput;
 }
 
 
+playGame = (playerChoice, cpuChoice) => {
+    console.log(playerChoice);
+    console.log(cpuChoice)
+
+    if (playerChoice === cpuChoice){
+        console.log("Draw! You both chose " + playerChoice)
+    } else if (playerChoice === "Rock" && cpuChoice === "Paper"){
+        console.log("You lose! " + cpuChoice + " beats " + playerChoice)
+    } else if (playerChoice === "Paper" && cpuChoice === "Scissors"){
+        console.log("You lose! " + cpuChoice + " beats " + playerChoice)
+    } else if (playerChoice === "Scissors" && cpuChoice === "Rock"){
+        console.log("You lose! " + cpuChoice + " beats " + playerChoice)
+    } else if (playerChoice === "Rock" && cpuChoice === "Scissors"){
+        console.log("You win! " + playerChoice + " beats " + cpuChoice)
+    } else if (playerChoice === "Paper" && cpuChoice === "Rock"){
+        console.log("You win! " + playerChoice + " beats " + cpuChoice)
+    } else if (playerChoice === "Scissors" && cpuChoice === "Paper"){
+        console.log("You win! " + playerChoice + " beats " + cpuChoice)
+    } else{
+        console.log("Unexpected error");
+    }
+
+}
 
 
 
-playerSelection();
+playGame(playerSelection(), computerPlays())
+
